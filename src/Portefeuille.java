2bba9b0a385bf9f3620781e45e6cd07924a4794c
Kseniia Pavlova <91989463+KsenaPav@users.noreply.github.com>
2023-04-11 11:16:01 +0200

    Update Portefeuille.java

--- src/Portefeuille.java
+++ src/Portefeuille.java
@@ -24,7 +24,6 @@ public class Portefeuille {
         **/
       if ( destination      == null              ) return false;
       if ( montantJetons     > this.montant      ) return false;
-      if ( destination.getNom().equals(this.getNom())) return false;
       
       this       .montant -= montantJetons;
       destination.montant += montantJetons;
